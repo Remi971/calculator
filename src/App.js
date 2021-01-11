be173c1be3bb943cee19afd53d1f1aade40d25e4
Remi Bhagalou <remi.bha@gmail.com>
2021-01-11 22:09:20 +0100

    Unexpected token in App.js..

--- src/App.js
+++ src/App.js
@@ -6,29 +6,49 @@ class App extends React.Component {
     super(props);
     this.state = {
       input: '0',
-      output: ''
+      operation: '',
+      output: '',
+      result:0
     };
   }
 
   operation = (e) => {
-    let input = this.state.input
-    if (input.match(/[/+*]$/)) {
-      if (input[input.length - 1] === e.target.innerHTML) {
-        return
+    if (this.state.output === '') {
+      return
+    }
+    const operators = {
+      '/': function(a,b){ return a/b},
+      '*': function(a,b){ return a*b},
+      '-': function(a,b){ return a-b},
+      '+': function(a,b){ return a+b},
+    }
+
+    if (this.state.output.match(/[/*-+]$/)) {
+      this.setState(
+        (prev) => ({
+          input: e.target.innerHTML,
+          output: prev.output.replace(/[/*-+]$/, e.target.innerHTML),
+          operation: e.target.innerHTML,
+          })
+        )
+      } else if (this.state.result === 0){
+        this.setState(
+          (prev) => ({
+            input: e.target.innerHTML,
+            output: prev.output + e.target.innerHTML,
+            operation: e.target.innerHTML,
+            result: prev.input
+          })
       } else {
         this.setState(
           (prev) => ({
-          input: prev.input + e.target.innerHTML,
-          output: prev.input + e.target.innerHTML
-        }))
+            input: e.target.innerHTML,
+            output: prev.output + e.target.innerHTML,
+            operation: e.target.innerHTML,
+            result: operators[prev.operation](prev.result, prev.input),
+            })
+        )
       }
-    }else {
-      this.setState(
-        (prev) => ({
-        input: prev.input + e.target.innerHTML,
-        output: prev.input + e.target.innerHTML
-      }))
-    }
   }
 
   handleClick = (e) => {
@@ -37,11 +57,18 @@ class App extends React.Component {
         input: e.target.innerHTML,
         output: e.target.innerHTML
       })
-    }else {
+    } else if (this.state.input.match(/^[/*-+]/)) {
+      this.setState(
+        (prev) => ({
+        input: e.target.innerHTML,
+        output: prev.output + e.target.innerHTML
+        })
+      )
+    } else {
       this.setState(
         (prev) => ({
           input: prev.input + e.target.innerHTML,
-          output: prev.input + e.target.innerHTML})
+          output: prev.output + e.target.innerHTML})
       );
     }
   }
@@ -49,7 +76,12 @@ class App extends React.Component {
   clear = () => {
     this.setState({
       input: '0',
-      output: ''});
+      output: ''
+    });
+  }
+
+  equal = () => {
+    console.log(this.state.result);
   }
 
   render() {
@@ -74,7 +106,7 @@ class App extends React.Component {
           <button onClick={this.handleClick} id='one' className='num'>1</button>
           <button onClick={this.handleClick} id='two' className='num'>2</button>
           <button onClick={this.handleClick} id='three' className='num'>3</button>
-          <button id='equals' className='egal'>=</button>
+          <button onClick={this.equal} id='equals' className='egal'>=</button>
           <button onClick={this.handleClick} id='zero' className='num zero'>0</button>
           <button id='decimal' className='num'>.</button>
         </div>
